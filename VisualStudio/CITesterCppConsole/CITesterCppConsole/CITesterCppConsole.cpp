fa8a630d62e59d85b9d9e308cf55e90b4a853289
mdj982 <maodejun001@hotmail.com>
2021-09-12 04:07:16 +0900

    add CITesterCppConsole

---
+++ VisualStudio/CITesterCppConsole/CITesterCppConsole/CITesterCppConsole.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cassert>
+
+int add(int x, int y) {
+	return x + y;
+}
+
+void testAddConst() {
+	const int x = 2;
+	const int y = 3;
+	int z = add(x, y);
+	assert(5 == z);
+}
+
+void testAddCin() {
+	int x, y;
+	std::cin >> x >> y;
+	int z = add(x, y);
+	assert(x + y == z);
+}
+
+int main(int argc, char *argv[])
+{
+	testAddConst();
+	// testAddCin();
+	std::cout << "ok" << std::endl;
+	return 0;
+}
